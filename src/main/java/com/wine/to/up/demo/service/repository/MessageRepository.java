797dae6e0cfb0e70ed6e09e2293df96f47961bdc
Elina <ehlinashin@yandex.ru>
2020-09-15 00:10:43 +0300

    add endpoint

--- src/main/java/com/wine/to/up/demo/service/repository/MessageRepository.java
+++ src/main/java/com/wine/to/up/demo/service/repository/MessageRepository.java
@@ -1,11 +1,19 @@
 package com.wine.to.up.demo.service.repository;
 
 import com.wine.to.up.demo.service.domain.entity.Message;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message, UUID> {
+    /**
+     * Define our custom method using HQL language
+     * @return list of distinct message stored in DB
+     */
+    @Query("select distinct m.content from Message m")
+    List<String> findDistinctContent();
 }
