e6298dac2fa525fbd6daa600daf6d9fb195f5251
Elina <ehlinashin@yandex.ru>
2020-09-15 14:01:03 +0300

    limit results

--- src/main/java/com/wine/to/up/demo/service/repository/MessageRepository.java
+++ src/main/java/com/wine/to/up/demo/service/repository/MessageRepository.java
@@ -1,6 +1,8 @@
 package com.wine.to.up.demo.service.repository;
 
 import com.wine.to.up.demo.service.domain.entity.Message;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,10 @@ public interface MessageRepository extends CrudRepository<Message, UUID> {
      * Define our custom method using HQL language
      * @return list of distinct message stored in DB
      */
-    @Query("select distinct m.content from Message m")
-    List<String> findDistinctContent();
+    @Query("SELECT DISTINCT m.content FROM Message m")
+    List<String> findDistinctContent(Pageable pageable);
+
+    default List<String> findDistinctContent() {
+        return findDistinctContent(PageRequest.of(0, 5));
+    }
 }
